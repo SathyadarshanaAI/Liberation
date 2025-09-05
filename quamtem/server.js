96530ac3587959836b42bc64c17be4f4c199dfbe
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-09-05 18:42:26 +0530

    Update CORS comment for clarity

--- quamtem/server.js
+++ quamtem/server.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch'); // Install: npm i node-fetch@2
 const app = express();
 const PORT = 3000;
 
-// Enable CORS (browser compatibility, optional)
+// Enable CORS (optional)
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
