479a25f610230ef0d6726377b8d50cca128f0f58
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-10-14 14:24:06 +0530

    Refactor service worker code for readability

--- palmistry/sw.js
+++ palmistry/sw.js
@@ -1,24 +1,18 @@
-// sw.js
-const CACHE = "qpa-v46-" + (self.crypto?.randomUUID?.() || Date.now());
-const ASSETS = ["./","./index.html","./app.js","./manifest.json"];
+const CACHE="qpa-v46-"+(self.crypto?.randomUUID?.()||Date.now());
+const ASSETS=["./","./index.html","./app.js","./manifest.json"];
 
-self.addEventListener("install", (e) => {
+self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-  ).then(()=>self.clients.claim()));
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))
+  self.clients.claim();
 });
-self.addEventListener("fetch", (e) => {
-  if (e.request.method!=="GET") return;
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
   e.respondWith(
-    caches.match(e.request).then(cached =>
-      cached || fetch(e.request).then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-        return res;
-      }).catch(()=>caches.match("./index.html"))
-    )
+    caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{
+      const copy=r.clone(); caches.open(CACHE).then(cc=>cc.put(e.request,copy)); return r;
+    }).catch(()=>caches.match("./index.html")))
   );
 });
