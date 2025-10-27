656e99fa52a8a2f7633a96b41e01a1a901556ad1
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-10-27 20:03:21 +0530

    Refactor analyzeRealPalm function parameters

--- palmistry/fusion.js
+++ palmistry/fusion.js
@@ -1,8 +1,8 @@
 import { generatePalmReport } from "./report.js";
 import { detectLines } from "./vision-map.js";
 
-export async function analyzeRealPalm(img,side){
-  const vision = await detectLines(img);
-  const report = await generatePalmReport(vision,side);
+export async function analyzeRealPalm(imgData, side) {
+  const vision = await detectLines(imgData);
+  const report = await generatePalmReport(vision, side);
   return `<div class="miniReport">${report}</div>`;
 }
