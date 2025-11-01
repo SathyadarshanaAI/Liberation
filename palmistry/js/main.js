5ae169d6cf6f6e0dd8c969b7f2131ee91eaae802
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-11-01 11:08:37 +0530

    Remove palm analysis and speech features
    Removed palm analysis and speech output for captured hands.

--- palmistry/js/main.js
+++ palmistry/js/main.js
@@ -1,11 +1,8 @@
 import { startCam, capture } from "./camera.js";
 import { drawAura } from "./aura.js";
 import { drawLines } from "./lines.js";
-import { analyzePalm } from "./aiBrain.js";
-import { speak } from "./voice.js";
 
 const statusEl = document.getElementById("status");
-const reportEl = document.getElementById("report");
 
 document.getElementById("startCamLeft").onclick = ()=>startCam("left", statusEl);
 document.getElementById("startCamRight").onclick = ()=>startCam("right", statusEl);
@@ -14,9 +11,6 @@ document.getElementById("captureLeft").onclick = ()=>{
   capture("left", ()=>{
     drawAura(document.getElementById("canvasLeftAura").getContext("2d"));
     drawLines(document.getElementById("canvasLeftLines").getContext("2d"));
-    const msg = analyzePalm("left", 0.83);
-    reportEl.innerText = msg;
-    speak("Left hand analyzed. Compassion and healing karma detected.");
   });
 };
 
@@ -24,8 +18,5 @@ document.getElementById("captureRight").onclick = ()=>{
   capture("right", ()=>{
     drawAura(document.getElementById("canvasRightAura").getContext("2d"));
     drawLines(document.getElementById("canvasRightLines").getContext("2d"));
-    const msg = analyzePalm("right", 0.91);
-    reportEl.innerText = msg;
-    speak("Right hand analyzed. Purpose and Dharma alignment detected.");
   });
 };
