e2c414aac0d001ffb384cb91c7cf83e8d1ed94d7
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-11-13 13:23:53 +0530

    Delete palmistry/modules/bus.js

--- palmistry/modules/bus.js
+++
@@ -1,6 +0,0 @@
-const listeners=new Map();
-export function on(t,fn){
-  if(!listeners.has(t))listeners.set(t,new Set());
-  listeners.get(t).add(fn);
-}
-export function emit(t,p){listeners.get(t)?.forEach(fn=>fn(p));}
