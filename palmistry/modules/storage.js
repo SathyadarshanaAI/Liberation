aef27fada212556fbb54035aa42847248a94fff7
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-11-13 13:38:03 +0530

    Delete palmistry/modules/storage.js

--- palmistry/modules/storage.js
+++
@@ -1,8 +0,0 @@
-export function saveNote(note) {
-  let notes = JSON.parse(localStorage.getItem("palmNotes")||"[]");
-  notes.push(note);
-  localStorage.setItem("palmNotes", JSON.stringify(notes));
-}
-export function getNotes() {
-  return JSON.parse(localStorage.getItem("palmNotes")||"[]");
-}
