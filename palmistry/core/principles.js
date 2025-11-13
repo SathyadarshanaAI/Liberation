18a62774fb2632e572e5b305aa60926185ef6ad4
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-11-13 07:22:35 +0530

    Add principles.js for universal palmistry principles

---
+++ palmistry/core/principles.js
@@ -0,0 +1,6 @@
+// Universal Palmistry Principles Loader
+
+export const PRINCIPLES = {
+  fiveElements: ["Earth", "Water", "Fire", "Air", "Space"],
+  mounts: ["Jupiter", "Saturn", "Apollo", "Mercury", "Moon", "Venus", "Mars"]
+};
